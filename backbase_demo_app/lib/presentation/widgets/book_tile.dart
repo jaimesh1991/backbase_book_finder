b9b4ec1a56daf9a8b90af44eb26fade08358431f
Jaimesh Patel <jaimesh1991@gmail.com>
2025-07-23 07:44:01 +0530

    User cached network image to download image in background.

--- backbase_demo_app/lib/presentation/widgets/book_tile.dart
+++ backbase_demo_app/lib/presentation/widgets/book_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 import '../../domain/entities/book_entity.dart';
@@ -14,13 +15,23 @@ class BookTile extends StatelessWidget {
         width: 50,
         height: 70,
         child: book.coverUrl.isNotEmpty
+            ? CachedNetworkImage(
+                imageUrl: book.coverUrl,
+                fit: BoxFit.cover,
+                placeholder: (context, url) =>
+                    const Icon(Icons.image, size: 40, color: Colors.grey),
+                errorWidget: (context, url, error) =>
+                    const Icon(Icons.broken_image),
+              )
+            : const Icon(Icons.book, size: 40),
+        /*book.coverUrl.isNotEmpty
             ? Image.network(
           book.coverUrl,
           fit: BoxFit.cover, // Ensures image fills the space
           errorBuilder: (context, error, stackTrace) =>
           const Icon(Icons.broken_image),
         )
-            : const Icon(Icons.book, size: 40),
+            : const Icon(Icons.book, size: 40),*/
       ),
       title: Text(book.title),
       subtitle: Text(book.author),
